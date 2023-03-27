1e369ca69b7a8d6d25192cec1848cbdd599f4204
Brian McCollum <brianmccollum@unm.edu>
2023-03-27 10:27:54 -0600

    finished training profiling

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import torch
+import torchvision
 from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
+# import torchvision.transforms as transforms
+from tqdm import tqdm
+
+from torch.profiler import profile, record_function, ProfilerActivity
 
 from whale_classifier.classifier import WhaleClassifier
 from whale_classifier.dataset import HappyWhaleDataset
@@ -17,7 +21,7 @@ test_img_path = "data/whales/test/"
 
 ### HYPER PARAMETERS (change to config file) ###
 EPOCHS = 10
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 LEARNING_RATE = 0.001
 
 # def parse_args():
@@ -43,14 +47,35 @@ def main():
     device = torch.device(device)
 
     # Define image transform
-    transform = transforms.Compose([
-        transforms.Normalize((127.5, 127.5, 127.5), (127.5, 127.5, 127.5)),
-        transforms.Resize((600, 600), antialias=True)
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.Normalize((127.5, 127.5, 127.5), (127.5, 127.5, 127.5)),
+        torchvision.transforms.Resize((224, 224), antialias=True)
     ])
 
+    # # Load dataset
+    # print("loading dataset...")
+    # annotation_file = pd.read_csv(train_annotation_path)
+    # if not os.path.exists("data/whales/class_list.txt"):
+    #     classes = [x for x in annotation_file["Id"].unique()]
+    #     with open("data/whales/class_list.txt", "w") as f:
+    #         for x in classes:
+    #             f.write(f"{x}\n")
+    # else:
+    #     with open("data/whales/class_list.txt", "r") as f:
+    #         classes = f.read().splitlines()
+    # print(f"[{len(classes)}] number of classes detected")
+
+    # # Create onehot labels
+    # print("creating onehot labels...")
+    # for index, (id, class_label) in annotation_file.iterrows():
+    #     onehot = np.zeros(len(classes), dtype=np.float32)
+    #     onehot[classes.index(class_label)] = 1
+    #     annotation_file.at[index, "Id"] = onehot
+
     # Load dataset
+    print("loading dataset...")
     annotation_file = pd.read_csv(train_annotation_path)
-    if not (os.path.exists("data/class_list.txt") or (os.path.isfile())):
+    if not os.path.exists("data/class_list.txt"):
         classes = [x for x in annotation_file["Id"].unique()]
         with open("data/class_list.txt", "w") as f:
             for x in classes:
@@ -60,38 +85,49 @@ def main():
             classes = f.read().splitlines()
     print(f"[{len(classes)}] number of classes detected")
 
-    # Create onehot labels
+    # Load image, perform normalize and transform, and create onehot labels
     for index, (id, class_label) in annotation_file.iterrows():
+        # image
+        # image = torchvision.io.read_image(os.path.join(train_img_path, id), mode=torchvision.io.ImageReadMode.RGB).float()
+        # image = torchvision.transforms.Normalize((127.5, 127.5, 127.5), (127.5, 127.5, 127.5))(image)
+        # image = torchvision.transforms.Resize((224, 224), antialias=True)(image)
+        # images.append(image)
+
+        # label
         onehot = np.zeros(len(classes), dtype=np.float32)
         onehot[classes.index(class_label)] = 1
+        # labels.append(onehot)
         annotation_file.at[index, "Id"] = onehot
-    # annotation_file.to_csv("data/train_onehot.csv")
-        # f.write(f"{id},[{','.join([str(x) for x in onehot])}]\n")
 
     train_dataset = HappyWhaleDataset(annotation_file, train_img_path, transform)
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=12, pin_memory=True)
 
     test_dataset = HappyWhaleDataset(annotation_file, train_img_path, transform)
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    test_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=12, pin_memory=True)
 
     # Create model, optimizer, and loss
+    print("creating model...")
     whale_classifier = WhaleClassifier(len(classes)).to(device)
+    whale_classifier = torch.compile(whale_classifier)
     optimizer = torch.optim.Adam(whale_classifier.parameters(), lr=LEARNING_RATE)
     criterion = torch.nn.CrossEntropyLoss()
 
     # Train
     # whale_classifier.train(train_dataloader)
-    for epoch in range(EPOCHS): # loop over the dataset multiple times
+    print("training model...")
+    # whale_classifier.train()
 
+    for epoch in range(EPOCHS): # loop over the dataset multiple times
         running_loss = 0.0
-        for i, data in enumerate(train_dataloader, 0):
+        for data in (progress_bar := tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{EPOCHS}")):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+            if device != torch.device("cpu"):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
             # zero the parameter gradients
-            optimizer.zero_grad()
+            optimizer.zero_grad(set_to_none=True)
 
             # forward + backward + optimize
             outputs = whale_classifier(inputs)
@@ -101,14 +137,12 @@ def main():
 
             # print statistics
             running_loss += loss.item()
-            print(i)
-            # if i % 2000 == 1999:    # print every 2000 mini-batches
-            #     print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            #     running_loss = 0.0
+            progress_bar.set_postfix(loss=f"{loss.item():.3f}")
+            # print("\r", f"Epoch: {epoch+1}/{EPOCHS} | Batch: {i+1}/{len(train_dataloader)} | ETA: {} | Loss: {running_loss / (i+1):.3f}", end="")
 
     print('Finished Training')
     # Save model (Saves entire model and not just weights, use whale_classifier.state_dict() to just save weights)
-    torch.save(whale_classifier, ".pt")
+    # torch.save(whale_classifier, ".pt")
 
     # Run inference (for now just run on train set since we dont have test labels)
     # prepare to count predictions for each class
@@ -119,6 +153,10 @@ def main():
     with torch.no_grad():
         for data in test_dataloader:
             images, labels = data
+            if device != torch.device("cpu"):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
             outputs = whale_classifier(images)
             _, predictions = torch.max(outputs, 1)
             # collect the correct predictions for each class
@@ -133,14 +171,8 @@ def main():
         accuracy = 100 * float(correct_count) / total_pred[classname]
         print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
 
-    # Plot and save stats
-
-
-    return
+    # # Plot and save stats
 
 if __name__ == "__main__":
     # args = parse_args()
-
-
-
     main()
--- whale_classifier/dataset.py
+++ whale_classifier/dataset.py
@@ -4,21 +4,22 @@ from torchvision.io import read_image, ImageReadMode
 from torch.utils.data import Dataset
 
 class HappyWhaleDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.onehot_labels = annotations_file
+    def __init__(self, annotation_file, img_dir, transform=None, target_transform=None):
+        self.annotation_file = annotation_file
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.onehot_labels)
+        return len(self.annotation_file)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.onehot_labels.iloc[idx, 0])
+        img_path = os.path.join(self.img_dir, self.annotation_file.iloc[idx, 0])
         image = read_image(img_path, mode=ImageReadMode.RGB)
-        label = self.onehot_labels.iloc[idx, 1]
+        label = self.annotation_file.iloc[idx, 1]
         if self.transform:
             image = self.transform(image.float())
         if self.target_transform:
             label = self.target_transform(label)
+
         return image, label
